343228109521d4c87b942a3b87d39dacbb1f2f79
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-03-08 11:10:26 +0100

    Add validation tests to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+    validates :username, presence: true, length: { maximum: 12 }, uniqueness: { case_sensitive: false }
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :password, presence: true, length: { maximum: 6 }
 end
