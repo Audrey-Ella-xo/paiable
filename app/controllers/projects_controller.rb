6514ed8205addba2c3be83c8a15a577aa72556ce
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-03-09 09:54:32 +0100

    Make create and index actions sfor projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,37 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[edit update destroy]
   def index
+    @projects = Project.all
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+
+    if @project.save
+      flash[:info] = 'project Created'
+      redirect_to projects_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
-  def show
+  def update
+  end
+
+  private 
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :worked_hours, :user_id)
   end
 end
