2b26f3f1db1d7636cb78f035dd5541554d0c8fe2
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-03-13 09:35:07 +0100

    Add fontawesome icons to view and remove unnecessary files

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,5 +3,41 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validation tests' do
+    subject { Group.new }
+
+    # it 'valid with valid attributes' do
+    #   subject.name = 'Any name'
+    #   subject.icon = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+    #   expect(subject).to be_valid
+    # end
+
+    it 'not valid without a name' do
+      subject.name = 'Any last name'
+      expect(subject).to_not be_valid
+    end
+
+    it 'not valid without an icon' do
+      subject.icon = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+      expect(subject).to_not be_valid
+    end
+
+    it 'not valid without valid attributes' do
+      expect(subject).to_not be_valid
+    end
+
+    it 'creates a group' do
+      expect { Group.create(name: 'Chifumnanya', lastname: 'Peña') }.not_to raise_error
+    end
+
+    it 'expects groupname to not be too long' do
+      subject.groupname = 'a' * 15
+      expect(subject).to_not be_valid
+    end
+
+    it 'expects password to not be too long' do
+      subject.password = 'a' * 8
+      expect(subject).to_not be_valid
+    end
+  end
 end
